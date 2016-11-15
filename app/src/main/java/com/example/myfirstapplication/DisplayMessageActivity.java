e4534baaf30ffdc33f5f1f078f4ff6bb93860fb6
n8behavior <mike@thesandmans.com>
2016-11-14 23:04:36 -0500

    Supporting Different Platform Versions
    https://developer.android.com/training/basics/supporting-devices/platforms.html

--- app/src/main/java/com/example/myfirstapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/example/myfirstapplication/DisplayMessageActivity.java
@@ -2,6 +2,7 @@ package com.example.myfirstapplication;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -11,6 +12,9 @@ public class DisplayMessageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setTheme(R.style.SimpleDiaglog);
+        }
         setContentView(R.layout.activity_display_message);
 
         Intent intent = getIntent();
